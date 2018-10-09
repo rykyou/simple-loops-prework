acc6c38b1e238a9bc2a90087fc19acbf4b948c18
Robin Kim <rkim1214@gmail.com>
2018-10-09 15:15:29 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,9 +1,9 @@
 def loop_iterator(number_of_times)
   counter = 0
   loop do
-    counter += 1
     puts "Welcome to Flatiron School's Web Development Course!"
-    break if counter > number_of_times
+    counter += 1 
+    break if counter == number_of_times
   end
 end
 
